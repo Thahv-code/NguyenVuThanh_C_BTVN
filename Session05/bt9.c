313127d187adb7fd503a7d235a944d5ea377bc08
Thahv-code <vuthanhnguyen946@gmail.com>
2025-10-28 23:49:30 +0700

    update

--- Session05/bt9.c
+++ Session05/bt9.c
@@ -3,11 +3,43 @@
 int main()
 {
     float heSoLuong;
-    int ngayCong;
-    int chucVu;
-    double phuCap = 0.0;
-    double thuong = 0.0;
-    double luongCoBan, luongTong;
+    int ngayCong, chucVu;
+    double phuCap = 0, thuong = 0, luong = 0;
+    const double LUONG_CO_BAN_THEO_NGAY = 160000;
 
+    printf("Nhap he so luong: ");
+    scanf("%f", &heSoLuong);
 
+    printf("Nhap so ngay cong trong thang: ");
+    scanf("%d", &ngayCong);
+
+    printf("Nhap chuc vu (1: Nhan vien, 2: To truong, 3: Quan ly): ");
+    scanf("%d", &chucVu);
+
+    switch (chucVu)
+    {
+    case 1:
+        phuCap = 500000;
+        break;
+    case 2:
+        phuCap = 1000000;
+        break;
+    case 3:
+        phuCap = 2000000;
+        break;
+    default:
+        printf("Chuc vu khong hop le.\n");
+        return 1; 
+    }
+
+    if (ngayCong > 26)
+    {
+        thuong = (ngayCong - 26) * 200000;
+    }
+
+    luong = ngayCong * LUONG_CO_BAN_THEO_NGAY * heSoLuong + phuCap + thuong;
+
+    printf("\nLuong nhan duoc la: %.2f VND\n", luong);
+
+    return 0;
 }
